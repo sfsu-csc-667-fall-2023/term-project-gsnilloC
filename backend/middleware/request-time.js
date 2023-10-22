df064e41c07f1f9adf4965e634be25605d3f1790
Collins Gichohi <cgichohi2018@gmail.com>
2023-10-21 20:54:11 -0700

    setting up still

---
+++ backend/middleware/request-time.js
@@ -0,0 +1,7 @@
+const requestTime = (request, response, next) => {
+  console.log(`Request received at ${Date.now()}: ${request.method}`);
+  
+  next();
+};
+
+module.exports = requestTime;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
+const path = require("path");
+const rootRoutes = require("./routes/root");
+const createError = require("http-errors");
+//const requestTime = require("./backend/middleware/request-time");
+
+
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
-});
+//app.use(requestTime);
+
+app.use("/", rootRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
+});
+
+app.use((request, response, next) => {
+  next(createError(404));
 });
